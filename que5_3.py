2b2629a5eda73b49bde0af1d975ef46469867f75
sakhi20-lab <sakshisawai30@gmail.com>
2025-05-23 23:37:43 +0530

    Add files via upload

---
+++ que5_3.py
@@ -0,0 +1,13 @@
+def main():
+    print("Enter the age of a Voter")
+    i = int(input())
+
+    if(i>=18):
+        print("eligible for voting")
+
+    else:
+        print(" not eligible for voting")
+
+
+if __name__=="__main__":
+    main()
