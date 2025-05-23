8643407b446eb9afbb218db389ab0e3e8d1093cf
sakhi20-lab <sakshisawai30@gmail.com>
2025-05-23 23:35:49 +0530

    Add files via upload

---
+++ que5_2.py
@@ -0,0 +1,15 @@
+def main():
+   print("Enter character")
+   ch = input()
+
+   if(ch=='a' or ch=='e' or ch=='i' or ch=='u' or ch=='o' or ch=='A'or ch=='I' or ch=='E' or ch=='O' or ch=='U'):
+       print ("character is vowel")
+
+   else:
+       print("character is consonant")
+       
+       
+
+
+if __name__=="__main__":
+    main()
