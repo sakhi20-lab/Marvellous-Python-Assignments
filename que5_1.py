bb0420722940f6548607367e31c19bb423010db9
sakhi20-lab <sakshisawai30@gmail.com>
2025-05-23 23:33:32 +0530

    Add files via upload

---
+++ que5_1.py
@@ -0,0 +1,17 @@
+def main():
+    int1 = int(input("Enter first number"))
+    int2 = int(input("Enter second number"))
+
+    SUM = int1 + int2
+    SUB = int2 - int2
+    MULT = int1 * int2
+    DIV = int1/ int2
+
+    print("Sum of two number is:",SUM)
+    print("Difference of two number is:",SUB)
+    print("Multipication  of two number is:",MULT)
+    print("Division of two number is:",DIV)
+
+
+if __name__=="__main__":
+    main()
